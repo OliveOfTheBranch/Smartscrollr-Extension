7fb9cb8c0b73b1f921e62f0ca008a2a128172275
OliveOfTheBranch <williamdk.101@gmail.com>
2026-08-02 00:55:57 +1200

    feat: set subject in extension
    Uses search params to set the subject in the React app via the extension.

--- popup.js
+++ popup.js
@@ -1,4 +1,11 @@
-document.getElementById("smartscrollr_url").addEventListener('click', function() {
+/*document.getElementById("smartscrollrUrl").addEventListener('click', function() {
     chrome.tabs.create({url: "https://twitch.tv/osunzstreams"}); //replace url
+    window.close();
+})*/
+
+document.getElementById("saveButton").addEventListener('click', function() {
+    const input = document.getElementById('userInput').value;
+    chrome.tabs.create({url: "http://localhost:5173/?subject=" + input}); //replace url
+    //alert('Data saved successfully!');
     window.close();
 })
